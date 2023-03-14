6280c1a5a5d7d9da170f5c0121103ebf9d2100e2
Elarodas <patrick.volpert@gmx.de>
2023-03-14 16:44:03 +0100

    Remove Plans from Video

--- images/client/src/components/Video/Videos.tsx
+++ images/client/src/components/Video/Videos.tsx
@@ -5,27 +5,6 @@ import { BroadcastWrapper } from '../../wrappers/BroadcastChannel';
 import VideosDisplay from './VideosDisplay';
 
 export default function Videos() {
-    // P2P Calls + Friends List
-    // https://reactrouter.com/en/main/start/tutorial
-    // Display as Sidebar
-    // P2P Calls: Form with Text Input with value UserId + Submit Button --> Action to /p2p/
-
-    //loader --> FriendsList: List of *fetched* friends + Call Button  + (fetched Online Status)
-
-    // Conference Call
-
-    // Display Centered
-    // Form : ConferenceName + Submit Button --> Action to /conference
-
-    // Call Options
-    // https://web.dev/building-a-settings-component/
-    // Display as Modal
-    // Activate Modal by pressing options on button on the top right
-
-    //Options:  Show Video on Start, Activate Microphon on Start
-
-    // Local Video
-    // TODO Give user Option what should be streamed
     const mediaConstraints: MediaStreamConstraints = {
         video: true,
     };
@@ -47,7 +26,6 @@ export default function Videos() {
     const configuration = {};
     const rtcPeer = useRTCPeer({
         configuration,
-        // TODO: Add ability to handle stream=null
         stream: localVideo.stream as MediaStream,
         signaling: broadCastWrapper,
         onRemoteTrackHandler,
