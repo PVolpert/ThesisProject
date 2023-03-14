26ff0eed037161d5a192c422e908bb0bf39e7039
Elarodas <patrick.volpert@gmx.de>
2023-03-14 16:41:34 +0100

    Add draft for Conference

---
+++ images/client/src/components/Call/Conference/Conference.tsx
@@ -0,0 +1,10 @@
+// TODO implement Conference
+
+/**
+ * Form with
+ * TextInput ConferenceName
+ * Button Generate Name
+ * Button : Submit Join Conference
+ */
+
+export default function Conference() {}
---
+++ images/client/src/components/Call/Conference/ConferenceDisplay.tsx
@@ -0,0 +1,7 @@
+/**
+ * Match space with sidebar
+ * Secondary Background
+ * Primary Box with Form inside centered
+ */
+
+export default function ConferenceDisplay() {}
