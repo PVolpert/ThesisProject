4c683087868422b128832a15f09afed80692615d
Elarodas <patrick.volpert@gmx.de>
2023-03-16 14:34:58 +0100

    Add css classes

--- images/client/src/components/Call/P2PSidebar/DirectCall/DirectCallDisplay.tsx
+++ images/client/src/components/Call/P2PSidebar/DirectCall/DirectCallDisplay.tsx
@@ -1,4 +1,5 @@
 import Button from '../../../UI/Button';
+import classes from './DirectCallDisplay.module.css';
 
 interface DirectCallDisplayProps {
     onRequestIATHandler: () => void;
@@ -8,12 +9,12 @@ export default function DirectCallDisplay({
     onRequestIATHandler,
 }: DirectCallDisplayProps) {
     return (
-        <form>
-            <div>
+        <form className={classes['form']}>
+            <div className={classes['text']}>
                 <label>Enter a valid caller Id</label>
                 <input name="userId" id="userId" type="text" />
             </div>
-            <div>
+            <div className={classes['controls']}>
                 <Button style="ternary" onClick={onRequestIATHandler}>
                     Test IAT
                 </Button>
