5ce6e226144386b73382a9bfac8f3c4c7bcc1b4e
Elarodas <patrick.volpert@gmx.de>
2023-03-16 14:34:35 +0100

    Add css class

--- images/client/src/components/Call/P2PSidebar/P2PSidebarDisplay.tsx
+++ images/client/src/components/Call/P2PSidebar/P2PSidebarDisplay.tsx
@@ -2,6 +2,7 @@ import AddAFriendButton from './AddAFriend/AddAFriendButton';
 import DirectCall from './DirectCall/DirectCall';
 import FriendsList from './Friend/FriendsList';
 
+import classes from './P2PSidebarDisplay.module.css';
 interface P2PSidebarDisplayProps {
     onRequestIATHandler: () => void;
 }
@@ -10,7 +11,7 @@ export default function P2PSidebarDisplay({
     onRequestIATHandler,
 }: P2PSidebarDisplayProps) {
     return (
-        <div>
+        <div className={classes['side-bar']}>
             <DirectCall onRequestIATHandler={onRequestIATHandler} />
             <FriendsList />
             <AddAFriendButton />
