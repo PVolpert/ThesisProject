f54812979c6e855dd491ca7319c4a54346e249f6
Elarodas <patrick.volpert@gmx.de>
2023-04-18 10:44:06 +0200

    Add logs

---
+++ images/api/logs/logs.go
@@ -0,0 +1,27 @@
+package logs
+
+import (
+	"api/env"
+	log "github.com/sirupsen/logrus"
+)
+
+func SetLogLevel() {
+	phase := env.Get("STAGE", "Production")
+	log.SetReportCaller(true)
+	switch phase {
+	case "Production":
+		setProdLog()
+	case "Development":
+		setDevLog()
+	}
+}
+
+func setProdLog() {
+	log.SetLevel(log.InfoLevel)
+	log.SetFormatter(&log.JSONFormatter{})
+}
+
+func setDevLog() {
+	log.SetLevel(log.DebugLevel)
+	log.SetFormatter(&log.TextFormatter{})
+}
