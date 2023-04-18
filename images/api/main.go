0a35b05ab84462c4eb7954dfd1e5abc2b69315f4
Elarodas <patrick.volpert@gmx.de>
2023-04-18 11:06:15 +0200

    Add /ictProviderInfo | Add Middleware

--- images/api/main.go
+++ images/api/main.go
@@ -1,50 +1,87 @@
 package main
 
 import (
+	"api/contexts"
 	"api/env"
 	"api/handlers"
+	"api/logs"
 	"fmt"
 
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/go-chi/docgen"
+	"github.com/go-chi/render"
+
 	log "github.com/sirupsen/logrus"
+)
+
+var (
+	routesEnv        = env.Get("ROUTES", "")
+	corsEnv   string = env.Get("CORS", "http://localhost:2000")
+	portEnv          = env.Get("PORT", "80")
 )
 
 func main() {
 
-	setLog()
+	logs.SetLogLevel()
 
-	log.Debug("Test the debug level")
 	router := chi.NewRouter()
+	// Logs the start and end of each request with the elapsed processing time
 	router.Use(middleware.Logger)
-	router.Get("/", handlers.RootHandler)
-	router.Get("/authServerInfo", handlers.GetAuthServerInfoHandler)
+	// Injects a request ID into the context of each request
+	router.Use(middleware.RequestID)
+	// Gracefully absorb panics and prints the stack trace
+	router.Use(middleware.Recoverer)
+	// Sets content type to JSON
+	router.Use(render.SetContentType(render.ContentTypeJSON))
 
-	port := env.Get("PORT", "80")
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{corsEnv}, // Use this to allow specific origin hosts
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+	// Authentication Token needed
+	router.Group(func(r chi.Router) {
 
-	log.Info("Go API started on Port", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
-}
+		// ? Past here can be routes
+		r.Route("/friends", func(r chi.Router) {
+			r.Get("/", handlers.DummyHandler)
 
-func setLog() {
-	phase := env.Get("STAGE", "Production")
-	log.SetReportCaller(true)
-	switch phase {
-	case "Production":
-		setProdLog()
-	case "Development":
-		setDevLog()
+			r.Route("/friendID", func(r chi.Router) {
+				r.Use(contexts.DummyCtx)
+				r.Get("/", handlers.DummyHandler)
+				r.Put("/", handlers.DummyHandler)
+				r.Delete("/", handlers.DummyHandler)
+			})
+		})
+
+		r.Route("/signaling", func(r chi.Router) {
+
+		})
+	})
+
+	// No Authentication Token needed
+	router.Group(func(r chi.Router) {
+		router.Get("/", handlers.RootHandler)
+		router.Get("/authProviderInfo", handlers.GetAuthProviderInfoHandler)
+		router.Get("/ictProviderInfo", handlers.GetICTProviderInfoHandler)
+	})
+
+	// Generate documentation for router if ROUTES Env is set
+	if len(routesEnv) != 0 {
+		fmt.Println(docgen.MarkdownRoutesDoc(router, docgen.MarkdownOpts{
+			ProjectPath: "github.com/elarodas/thesisproject/api",
+			Intro:       "generated REST docs for api",
+		}))
+		return
 	}
-}
 
-func setProdLog() {
-	log.SetLevel(log.InfoLevel)
-	log.SetFormatter(&log.JSONFormatter{})
-}
-
-func setDevLog() {
-	log.SetLevel(log.DebugLevel)
-	log.SetFormatter(&log.TextFormatter{})
+	log.Info(fmt.Sprintf("Go API started on Port %s", portEnv))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", portEnv), router))
 }
