50e140bff17311a6095e59dc6b3b80a96a95384e
Elarodas <patrick.volpert@gmx.de>
2023-05-12 14:01:56 +0200

    Add ict image

---
+++ images/ict/go.mod
@@ -0,0 +1,14 @@
+module ict
+
+go 1.20
+
+require github.com/golang-jwt/jwt/v4 v4.4.2
+
+require (
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/go-chi/docgen v1.2.0
+	github.com/mattn/go-sqlite3 v1.14.15
+	github.com/sirupsen/logrus v1.9.0
+)
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
---
+++ images/ict/logs/logs.go
@@ -0,0 +1,28 @@
+package logs
+
+import (
+	"ict/env"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func SetLogLevel() {
+	phase := env.Get("STAGE", "Development")
+	log.SetReportCaller(true)
+	switch phase {
+	case "Production":
+		setProdLog()
+	case "Development":
+		setDevLog()
+	}
+}
+
+func setProdLog() {
+	log.SetLevel(log.InfoLevel)
+	log.SetFormatter(&log.JSONFormatter{})
+}
+
+func setDevLog() {
+	log.SetLevel(log.DebugLevel)
+	log.SetFormatter(&log.TextFormatter{})
+}
