74dcd29210118b9f20a6b75aaa12023511ad6998
Elarodas <patrick.volpert@gmx.de>
2023-05-15 13:10:29 +0200

    Add context functionality

--- images/signaling/signalingServer/userId.go
+++ images/signaling/signalingServer/userId.go
@@ -2,15 +2,24 @@ package signalingServer
 
 import (
 	"context"
+	"fmt"
 	"math/rand"
+
+	"github.com/ElarOdas/ctxValueBuilder"
 )
 
 type userId string
 
-func idFromContext(ctx context.Context) userId {
+const userIdContextKey = "UserIDKey"
 
-	// TODO Add function that takes name from token
-	return userId(RandomString(10))
+func idFromContext(ctx context.Context) (userId, error) {
+	fromCtxFunc := ctxValueBuilder.FromContext[string, userId](fmt.Errorf("no userID found"))
+	return fromCtxFunc(ctx, userIdContextKey)
+}
+
+func idToContext(ctx context.Context, id userId) context.Context {
+	toCtxFunc := ctxValueBuilder.ToContext[string, userId]()
+	return toCtxFunc(ctx, userIdContextKey, id)
 }
 
 // ! For testing purposes only
