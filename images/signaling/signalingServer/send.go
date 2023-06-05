7e3e044a612f7fb9603c638c0b6a43903c68332c
Elarodas <patrick.volpert@gmx.de>
2023-06-05 12:25:21 +0200

    Add rate limiting

--- images/signaling/signalingServer/send.go
+++ images/signaling/signalingServer/send.go
@@ -1,15 +1,19 @@
 package signalingServer
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func (sig *signalingServer) sendToUser(id userId, msg message) error {
+func (sig *signalingServer) sendToUser(id *userId, msg message) error {
 	sig.subscribersMu.Lock()
 	defer sig.subscribersMu.Unlock()
 
-	target := sig.subscribers[id]
+	target := sig.subscribers[*id]
 	if target == nil {
 		return fmt.Errorf("no such sub online")
 	}
+
+	<-sig.rateLimiter.C
 
 	select {
 	case target.msgs <- msg:
@@ -22,6 +26,9 @@ func (sig *signalingServer) sendToUser(id userId, msg message) error {
 func (sig *signalingServer) broadcast(msg message) {
 	sig.subscribersMu.Lock()
 	defer sig.subscribersMu.Unlock()
+
+	<-sig.rateLimiter.C
+
 	for _, s := range sig.subscribers {
 		select {
 		case s.msgs <- msg:
