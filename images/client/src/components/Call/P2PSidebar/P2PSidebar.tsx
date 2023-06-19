8710ad6ea7a00515fdb47b2577e9423d1f09ab5e
PVolpert <patrick.volpert@gmx.de>
2023-06-19 13:38:27 +0200

    Remove old features

--- images/client/src/components/Call/P2PSidebar/P2PSidebar.tsx
+++ images/client/src/components/Call/P2PSidebar/P2PSidebar.tsx
@@ -1,37 +1,11 @@
-import { generateKeyPair, IDToken } from 'oauth4webapi';
-import { useRouteLoaderData } from 'react-router-dom';
-import useModal from '../../../hooks/useModal';
-import { useToken } from '../../../hooks/useToken';
-import OIDCProvider from '../../../wrappers/Auth/OIDCProvider';
 import P2PSidebarDisplay from './P2PSidebarDisplay';
 
 /**
  * Sidebar for P2P Calls on /call Page
  * Contains
- * - Direct Call
- * - FriendsList
- * - AddAFriend
- *
+ * - List of active users
  */
 
-export interface AddAFriendModal {
-    isModalShown: boolean;
-    hideModal: () => void;
-    showModal: () => void;
-}
-
 export default function P2PSidebar() {
-    const addAFriendModal = useModal({ shownInitial: false });
-
-    // TODO Complete ICT Request: Read about ICT Token Flow
-    async function onRequestICTHandler() {
-        const keyPair = await generateKeyPair('ES384', { extractable: true });
-    }
-
-    return (
-        <P2PSidebarDisplay
-            onRequestICTHandler={onRequestICTHandler}
-            addAFriendModal={addAFriendModal}
-        />
-    );
+    return <P2PSidebarDisplay />;
 }
--- images/client/src/components/Call/P2PSidebar/P2PSidebarDisplay.tsx
+++ images/client/src/components/Call/P2PSidebar/P2PSidebarDisplay.tsx
@@ -1,32 +1,12 @@
-import AddAFriend from './AddAFriend/AddAFriend';
-import AddAFriendButton from './AddAFriend/AddAFriendButton';
-import DirectCall from './DirectCall/DirectCall';
 import UserList from './Users/UserList';
-import { AddAFriendModal } from './P2PSidebar';
 
 import classes from './P2PSidebarDisplay.module.css';
-interface P2PSidebarDisplayProps {
-    onRequestICTHandler: () => void;
-    addAFriendModal: AddAFriendModal;
-}
+interface P2PSidebarDisplayProps {}
 
-export default function P2PSidebarDisplay({
-    onRequestICTHandler,
-    addAFriendModal,
-}: P2PSidebarDisplayProps) {
-    const {
-        isModalShown: isAddAFriendShown,
-        hideModal: hideAddAFriend,
-        showModal: showAddAFriend,
-    } = addAFriendModal;
+export default function P2PSidebarDisplay({}: P2PSidebarDisplayProps) {
     return (
         <div className={classes['side-bar']}>
-            {/* <DirectCall onRequestICTHandler={onRequestICTHandler} /> */}
             <UserList />
-            {/* <AddAFriendButton showAddAFriend={showAddAFriend} />
-            {isAddAFriendShown && (
-                <AddAFriend hideAddAFriend={hideAddAFriend} />
-            )} */}
         </div>
     );
 }
