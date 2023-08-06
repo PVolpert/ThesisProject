50056d8e25696e2696b7425bd493b76ca9ff16f6
PVolpert <patrick.volpert@gmx.de>
2023-08-06 13:09:01 +0200

    Add LoadStateICon component

---
+++ images/client/src/components/UI/LoadStateIcon.tsx
@@ -0,0 +1,76 @@
+import { ReactNode } from 'react';
+
+export type loadState = 'loading' | 'done' | 'failed';
+
+interface LoadStateIconProps {
+    children?: ReactNode;
+    className?: string;
+    loadState: loadState;
+}
+
+export default function LoadStateIcon({
+    children,
+    className = '',
+    loadState,
+}: LoadStateIconProps) {
+    switch (loadState) {
+        case 'loading':
+            return (
+                <svg
+                    className="w-6 text-white animate-spin "
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                >
+                    <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        stroke-width="4"
+                    ></circle>
+                    <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                </svg>
+            );
+
+        case 'done':
+            return (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4.5 12.75l6 6 9-13.5"
+                    />
+                </svg>
+            );
+        case 'failed':
+            return (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                    />
+                </svg>
+            );
+    }
+}
