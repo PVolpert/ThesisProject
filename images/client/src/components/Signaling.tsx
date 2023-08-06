04f2fa8ce99f0e86d6b588723ae4fc44d5c010e8
PVolpert <patrick.volpert@gmx.de>
2023-08-06 13:08:37 +0200

    Fix imports | Change connection status to svg

--- images/client/src/components/Signaling.tsx
+++ images/client/src/components/Signaling.tsx
@@ -3,13 +3,13 @@ import useSignaling from '../hooks/useSignaling';
 import { useCallback, useEffect } from 'react';
 
 import { useNavigate } from 'react-router-dom';
-import { useZustandStore } from '../stores/zustand/ZustandStore';
+import { useStore } from '../store/Store';
 import {
     Message,
     SdpMessage,
     createHangUpMessage,
-} from '../wrappers/Signaling/Messages';
-import { incomingCallHandler } from '../wrappers/Signaling/MessageHandlers';
+} from '../helpers/Signaling/Messages';
+import { incomingCallHandler } from '../helpers/Signaling/MessageHandlers';
 import { useToken } from '../hooks/useToken';
 
 export default function Signaling() {
@@ -18,10 +18,10 @@ export default function Signaling() {
         socket: { lastJsonMessage, sendJsonMessage, readyState },
     } = useSignaling({ socketUrl: signalingUrl });
     const navigate = useNavigate();
-    const isRTCConnectionActive = useZustandStore((state) => {
+    const isRTCConnectionActive = useStore((state) => {
         return state.isRTCConnectionActive;
     });
-    const setSdpOffer = useZustandStore((state) => {
+    const setSdpOffer = useStore((state) => {
         return state.setSdpOffer;
     });
 
@@ -62,12 +62,92 @@ export default function Signaling() {
 
     // * Translate ReadyState into string
     const connectionStatus = {
-        [ReadyState.CONNECTING]: 'Connecting',
-        [ReadyState.OPEN]: 'Open',
-        [ReadyState.CLOSING]: 'Closing',
-        [ReadyState.CLOSED]: 'Closed',
-        [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+        [ReadyState.CONNECTING]: (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.348 14.651a3.75 3.75 0 010-5.303m5.304 0a3.75 3.75 0 010 5.303m-7.425 2.122a6.75 6.75 0 010-9.546m9.546 0a6.75 6.75 0 010 9.546M5.106 18.894c-3.808-3.808-3.808-9.98 0-13.789m13.788 0c3.808 3.808 3.808 9.981 0 13.79M12 12h.008v.007H12V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                />
+            </svg>
+        ),
+        [ReadyState.OPEN]: (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.348 14.651a3.75 3.75 0 010-5.303m5.304 0a3.75 3.75 0 010 5.303m-7.425 2.122a6.75 6.75 0 010-9.546m9.546 0a6.75 6.75 0 010 9.546M5.106 18.894c-3.808-3.808-3.808-9.98 0-13.789m13.788 0c3.808 3.808 3.808 9.981 0 13.79M12 12h.008v.007H12V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                />
+            </svg>
+        ),
+        [ReadyState.CLOSING]: (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 3l8.735 8.735m0 0a.374.374 0 11.53.53m-.53-.53l.53.53m0 0L21 21M14.652 9.348a3.75 3.75 0 010 5.304m2.121-7.425a6.75 6.75 0 010 9.546m2.121-11.667c3.808 3.807 3.808 9.98 0 13.788m-9.546-4.242a3.733 3.733 0 01-1.06-2.122m-1.061 4.243a6.75 6.75 0 01-1.625-6.929m-.496 9.05c-3.068-3.067-3.664-7.67-1.79-11.334M12 12h.008v.008H12V12z"
+                />
+            </svg>
+        ),
+        [ReadyState.CLOSED]: (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 3l8.735 8.735m0 0a.374.374 0 11.53.53m-.53-.53l.53.53m0 0L21 21M14.652 9.348a3.75 3.75 0 010 5.304m2.121-7.425a6.75 6.75 0 010 9.546m2.121-11.667c3.808 3.807 3.808 9.98 0 13.788m-9.546-4.242a3.733 3.733 0 01-1.06-2.122m-1.061 4.243a6.75 6.75 0 01-1.625-6.929m-.496 9.05c-3.068-3.067-3.664-7.67-1.79-11.334M12 12h.008v.008H12V12z"
+                />
+            </svg>
+        ),
+        [ReadyState.UNINSTANTIATED]: (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 3l8.735 8.735m0 0a.374.374 0 11.53.53m-.53-.53l.53.53m0 0L21 21M14.652 9.348a3.75 3.75 0 010 5.304m2.121-7.425a6.75 6.75 0 010 9.546m2.121-11.667c3.808 3.807 3.808 9.98 0 13.788m-9.546-4.242a3.733 3.733 0 01-1.06-2.122m-1.061 4.243a6.75 6.75 0 01-1.625-6.929m-.496 9.05c-3.068-3.067-3.664-7.67-1.79-11.334M12 12h.008v.008H12V12z"
+                />
+            </svg>
+        ),
     }[readyState];
 
-    return <p>Status: {connectionStatus}</p>;
+    return (
+        <div className="flex flex-col md:flex-row place-items-center space-x-2">
+            <p className="hidden md:block">Status:</p>
+            {connectionStatus}
+        </div>
+    );
 }
