888ab6e80cbcd7ebd1febe1e9916e389cda68a14
PVolpert <patrick.volpert@gmx.de>
2023-08-06 13:01:42 +0200

    Switch card to tailwind

--- images/client/src/components/UI/Card.tsx
+++ images/client/src/components/UI/Card.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
-import classes from './Card.module.css';
-
 interface CardProps {
-    className ?: string
-    children : React.ReactNode
+    className?: string;
+    children: React.ReactNode;
 }
 
-function Card  ({className,children}:CardProps)  {
-  return <div className={`${classes.card} ${className}`}>{children}</div>;
-};
+function Card({ className, children }: CardProps) {
+    return (
+        <div
+            className={`flex flex-col md:flex-row space-y-10 md:space-y-0 bg-zinc-100 dark:bg-zinc-700 border border-zinc-500 shadow-xl rounded-2xl ${className}`}
+        >
+            {children}
+        </div>
+    );
+}
 
 export default Card;
