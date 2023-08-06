040402de48f4fb5783f97cfbc8379c3ffd7b6163
PVolpert <patrick.volpert@gmx.de>
2023-08-06 13:03:06 +0200

    Switch to tailwind

--- images/client/src/components/UI/OIDCProviderButton.tsx
+++ images/client/src/components/UI/OIDCProviderButton.tsx
@@ -1,29 +1,24 @@
-import classes from './OIDCProviderButton.module.css';
-
 interface OIDCProviderButtonProps {
     onClick: () => void;
     logo: URL;
     text: string;
-    isTokenActive: boolean;
+    isTokenActive?: boolean;
 }
 
 export default function OIDCProviderButton({
     onClick,
     logo,
     text,
-    isTokenActive,
+    isTokenActive = false,
 }: OIDCProviderButtonProps) {
     return (
-        <div className={classes.item}>
-            <button onClick={onClick} disabled={isTokenActive}>
-                <span
-                    className={classes['item__logo']}
-                    style={{
-                        backgroundImage: `url(${logo.href})`,
-                    }}
-                ></span>
-                <span className={classes['item__text']}>{text}</span>
-            </button>
-        </div>
+        <button
+            onClick={onClick}
+            disabled={isTokenActive}
+            className="flex  items-center justify-center w-full py-2 space-x-3 bg-zinc-100 dark:bg-zinc-600 border border-zinc-500 bg-inherit rounded shadow-sm hover:brightness-110 hover:shadow-lg hover:-translate-y-0.5 transition duration-1050"
+        >
+            <img src={logo.href} className="w-9" />
+            <span className="font-thin">{text}</span>
+        </button>
     );
 }
