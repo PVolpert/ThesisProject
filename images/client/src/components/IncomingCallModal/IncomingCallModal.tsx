542309b9aa985198d206fedfc0e60ab3c28f68f3
PVolpert <patrick.volpert@gmx.de>
2023-08-19 02:38:42 +0200

    Assign yes and no functions

--- images/client/src/components/IncomingCallModal/IncomingCallModal.tsx
+++ images/client/src/components/IncomingCallModal/IncomingCallModal.tsx
@@ -4,6 +4,7 @@ import Modal from '../UI/Modal';
 import ConfirmCaller from './IncomingCallSteps/ConfirmCaller';
 import ShowCallSteps from './IncomingCallSteps/ShowCallSteps';
 import IncomingCallLoadbar from './IncomingCallLoadBar';
+import { useNavigate } from 'react-router-dom';
 
 interface IncomingCallModalProps {
     children?: ReactNode;
@@ -12,16 +13,39 @@ interface IncomingCallModalProps {
 
 export default function IncomingCallModal({}: IncomingCallModalProps) {
     const stage = useStore((state) => state.incomingCallStage);
+
     const hideModal = useStore((state) => state.hideIncomingCallModal);
+    const setIncomingCallModalStage = useStore(
+        (state) => state.setIncomingCallModalStage
+    );
+    const navigate = useNavigate();
 
     return (
         <Modal onHideModal={hideModal}>
-            <div className="flex flex-col space-y-2">
+            <div className="flex flex-col p-4 space-y-2">
                 <div className="flex-1 flex ">
                     <IncomingCallLoadbar stage={stage} />
                 </div>
-                {stage == 0 && <ConfirmCaller />}
-                {stage == 1 && <ShowCallSteps />}
+                {stage == 0 && (
+                    <ConfirmCaller
+                        onClickYes={() => {
+                            setIncomingCallModalStage(1);
+                            navigate('/call/p2p');
+                        }}
+                        onClickNo={() => {}}
+                    />
+                )}
+                {stage == 1 && (
+                    <ShowCallSteps
+                        onClickYes={() => {
+                            hideModal();
+                        }}
+                        onClickNo={() => {
+                            hideModal();
+                            navigate('/call/');
+                        }}
+                    />
+                )}
             </div>
         </Modal>
     );
