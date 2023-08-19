ca0085688873d41b57cd86e6507a227b727c9b92
PVolpert <patrick.volpert@gmx.de>
2023-08-19 02:35:41 +0200

    Add values to incoming

--- images/client/src/store/slices/IncomingCallSlice.tsx
+++ images/client/src/store/slices/IncomingCallSlice.tsx
@@ -4,18 +4,29 @@ import { ICTAccessTokenSlice } from './ICTAccessTokenSlice';
 import { RTCConnectionSlice } from './RTCConnectionSlice';
 import { ModalSlice } from './ModalSlice';
 import { AccessTokenSlice } from './AccessTokenSlice';
-import { OutgoingCallSlice } from './OutgoingCallSlice';
+import { AsyncTaskState, OutgoingCallSlice } from './OutgoingCallSlice';
 
 interface State {
     incomingCallStage: 0 | 1;
+    answerLoadState: AsyncTaskState;
+    ictAnswerLoadState: AsyncTaskState;
+    answerSentLoadState: AsyncTaskState;
 }
 
-interface Actions {}
+interface Actions {
+    setAnswerLoadState: (newLoadState: AsyncTaskState) => void;
+    setICTAnswerLoadState: (newLoadState: AsyncTaskState) => void;
+    setAnswerSentLoadState: (newLoadState: AsyncTaskState) => void;
+    setIncomingCallModalStage: (newStage: 0 | 1) => void;
+}
 
 export interface IncomingCallSlice extends State, Actions {}
 
 const initialState: State = {
     incomingCallStage: 0,
+    answerLoadState: 'pending',
+    ictAnswerLoadState: 'pending',
+    answerSentLoadState: 'pending',
 };
 
 export const createIncomingCallSlice: StateCreator<
@@ -31,4 +42,12 @@ export const createIncomingCallSlice: StateCreator<
     IncomingCallSlice
 > = (set) => ({
     ...initialState,
+    setAnswerLoadState: (newLoadState) =>
+        set({ answerLoadState: newLoadState }),
+    setICTAnswerLoadState: (newLoadState) =>
+        set({ ictAnswerLoadState: newLoadState }),
+    setAnswerSentLoadState: (newLoadState) =>
+        set({ answerSentLoadState: newLoadState }),
+    setIncomingCallModalStage: (newStage) =>
+        set({ incomingCallStage: newStage }),
 });
