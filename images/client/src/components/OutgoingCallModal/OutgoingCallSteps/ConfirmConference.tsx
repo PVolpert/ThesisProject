17a8b70f5e71d61086f28770ec5273bbbd7892a9
PVolpert <patrick.volpert@gmx.de>
2023-09-29 15:48:04 +0200

    Change outgoing call modal

--- images/client/src/components/OutgoingCallModal/OutgoingCallSteps/ConfirmConference.tsx
+++ images/client/src/components/OutgoingCallModal/OutgoingCallSteps/ConfirmConference.tsx
@@ -2,24 +2,32 @@ import { Description, MainTitle } from '../../UI/Headers';
 import Button from '../../UI/Button';
 
 import { UserInfo } from '../../../helpers/Signaling/User';
+import UserBlock from '../../UI/UserBlock';
 
-interface ConfirmCallProps {
+interface ConfirmConferenceProps {
     onClickYes: () => void;
     onClickNo: () => void;
-    callee: UserInfo;
+    candidates: UserInfo[];
 }
 
-export default function ConfirmCall({
+export default function ConfirmConference({
     onClickNo,
     onClickYes,
-    callee,
-}: ConfirmCallProps) {
+    candidates,
+}: ConfirmConferenceProps) {
     return (
         <div className="flex flex-col space-y-4 p-4">
-            <MainTitle> Call {callee.username || 'Unkown'}? </MainTitle>
+            <MainTitle> Conference? </MainTitle>
             <Description>
-                Are you sure you want to call {callee.username || 'Unknown'}?
+                Are you sure you want to start a conference with the following
+                users?
             </Description>
+
+            <div className="flex flex-col space-y-2">
+                {candidates.map((candidate) => {
+                    return <UserBlock username={candidate.username} />;
+                })}
+            </div>
 
             <div className="flex flex-col md:flex-row md:space-x-4 md:space-y-0 space-y-4 justify-center">
                 <Button
